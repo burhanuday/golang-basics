c78d3238c40b9a2ea754be1d545ccd55bffb06ce
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2021-06-06 11:31:33 +0530

    template engine with go

--- basic-web-application/cmd/web/main.go
+++ basic-web-application/cmd/web/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/burhanuday/basic-web-application/pkg/config"
 	"github.com/burhanuday/basic-web-application/pkg/handlers"
+	"github.com/burhanuday/basic-web-application/pkg/render"
 )
 
 const port = ":5000"
 
 // main is the entrypoint
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+
+	if err != nil {
+		log.Fatal("Could not create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	http.ListenAndServe(port, nil)
 }
--- basic-web-application/pkg/handlers/handlers.go
+++ basic-web-application/pkg/handlers/handlers.go
@@ -3,15 +3,37 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/burhanuday/basic-web-application/pkg/config"
+	"github.com/burhanuday/basic-web-application/pkg/models"
 	"github.com/burhanuday/basic-web-application/pkg/render"
 )
 
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 // Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.html")
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
 }
 
 // About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.html")
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello again"
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
